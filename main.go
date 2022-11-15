f56dc663776e53fae86b500befc11ba9f2f0a150
Daniel Dias de Assumpção <dassump@gmail.com>
2022-11-15 19:39:34 -0300

    Separator length validation More simplified variable definitions

--- main.go
+++ main.go
@@ -16,43 +16,25 @@ import (
 )
 
 var (
-	app         string = "dotnet-appsettings-env"
-	version     string = "dev"
-	description string = "Convert .NET appsettings.json file to Kubernetes, Docker, Docker-Compose and Bicep environment variables."
-	site        string = "https://github.com/dassump/dotnet-appsettings-env"
-	info        string = "%s (%s)\n\n%s\n%s\n\n"
-	usage       string = "Usage of %s:\n"
+	app         = "dotnet-appsettings-env"
+	version     = "dev"
+	description = "Convert .NET appsettings.json file to Kubernetes, Docker, Docker-Compose and Bicep environment variables."
+	site        = "https://github.com/dassump/dotnet-appsettings-env"
 
-	file       string
-	file_name  string = "file"
-	file_value string = "./appsettings.json"
-	file_usage string = "Path to file appsettings.json"
-	file_error string = "%s\n"
+	file      = flag.String("file", "./appsettings.json", "Path to file appsettings.json")
+	output    = flag.String("type", "k8s", "Output to Kubernetes (k8s) / Docker (docker) / Docker Compose (compose) / Bicep (bicep)")
+	separator = flag.String("separator", "__", "Separator character(s)")
 
-	output       string
-	output_name  string = "type"
-	output_value string = "k8s"
-	output_usage string = "Output to Kubernetes (k8s) / Docker (docker) / Docker Compose (compose) / Bicep (bicep)"
-	output_error string = "Type %s is not valid\n"
-
-	separator       string
-	separator_name  string = "separator"
-	separator_value string = "__"
-	separator_usage string = "Separator character"
-
-	content              map[string]any
-	content_comments     string = `(?m:\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$)`
-	content_error        string = "%s in %s\n"
-	content_syntax_error string = "%s in %s\n\n... line %d, column %d\n%s >>> %s <<< %s\n...\n"
-	content_syntax_near  int64  = 60
-
-	variables = map[string]string{}
-	format    = map[string]string{
+	comments = `(?m:\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$)`
+	format   = map[string]string{
 		"k8s":     "- name: %q\n  value: %q\n",
 		"docker":  "%s=%q\n",
 		"compose": "%s: %q\n",
 		"bicep":   "{\nname: '%s'\nvalue: '%s'\n}\n",
 	}
+
+	objects   = make(map[string]any)
+	variables = make(map[string]string)
 )
 
 func init() {
@@ -60,69 +42,71 @@ func init() {
 	log.SetPrefix("Error: ")
 
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), info, app, version, description, site)
-		fmt.Fprintf(flag.CommandLine.Output(), usage, os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "%s (%s)\n\n%s\n%s\n\n", app, version, description, site)
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 
-	flag.StringVar(&file, file_name, file_value, file_usage)
-	flag.StringVar(&output, output_name, output_value, output_usage)
-	flag.StringVar(&separator, separator_name, separator_value, separator_usage)
-
 	flag.Parse()
 
-	if _, ok := format[output]; !ok {
-		log.Fatalf(output_error, output)
+	if _, ok := format[*output]; !ok {
+		log.Fatalln(*output, "is not valid type")
+	}
+
+	if len(*separator) < 1 {
+		log.Fatalln("separator cannot be an empty string")
 	}
 }
 
 func main() {
-	file_bytes, err := os.ReadFile(file)
+	content, err := os.ReadFile(*file)
 	if err != nil {
-		log.Fatalf(file_error, err)
+		log.Fatalln(err)
 	}
 
 	_, err = transform.NewReader(
-		bytes.NewReader(file_bytes),
+		bytes.NewReader(content),
 		unicode.BOMOverride(unicode.UTF8.NewDecoder()),
-	).Read(file_bytes)
+	).Read(content)
 	if err != nil {
-		log.Fatalf(file_error, err)
+		log.Fatalln(err)
 	}
 
-	file_bytes = regexp.MustCompile(content_comments).ReplaceAll(file_bytes, nil)
+	content = regexp.MustCompile(comments).ReplaceAll(content, nil)
 
-	decoder := json.NewDecoder(bytes.NewReader(file_bytes))
+	decoder := json.NewDecoder(bytes.NewReader(content))
 	decoder.UseNumber()
 
-	if err := decoder.Decode(&content); err != nil {
+	if err := decoder.Decode(&objects); err != nil {
 		switch err := err.(type) {
 		case *json.SyntaxError:
-			new_line := []byte("\n")
-			line := 1 + bytes.Count(file_bytes[:err.Offset], new_line)
-			column := int(err.Offset) - bytes.LastIndex(file_bytes[:err.Offset], new_line) - len(new_line)
+			newline := []byte("\n")
+			line := 1 + bytes.Count(content[:err.Offset], newline)
+			column := int(err.Offset) - bytes.LastIndex(content[:err.Offset], newline) - len(newline)
+			near := int64(60)
 
-			near_before := err.Offset - content_syntax_near
-			if err.Offset-content_syntax_near < 0 {
-				near_before = 0
+			before := err.Offset - near
+			if err.Offset-near < 0 {
+				before = 0
 			}
 
-			near_after := err.Offset + content_syntax_near
-			if err.Offset+content_syntax_near > int64(len(file_bytes)) {
-				near_after = int64(len(file_bytes))
+			after := err.Offset + near
+			if err.Offset+near > int64(len(content)) {
+				after = int64(len(content))
 			}
 
 			log.Fatalf(
-				content_syntax_error,
-				err, file, line, column,
-				file_bytes[near_before:err.Offset-1], file_bytes[err.Offset-1:err.Offset], file_bytes[err.Offset:near_after],
+				"%s in %s\n\n... line %d, column %d\n%s >>> %s <<< %s\n...\n",
+				err, *file, line, column,
+				content[before:err.Offset-1], content[err.Offset-1:err.Offset], content[err.Offset:after],
 			)
+
 		default:
-			log.Fatalf(content_error, err, file)
+			log.Fatalf("%s in %s\n", err, *file)
 		}
 	}
 
-	parser(content, variables, nil)
+	parser(objects, variables, nil)
 
 	keys := make([]string, 0, len(variables))
 	for key := range variables {
@@ -134,7 +118,7 @@ func main() {
 	})
 
 	for _, key := range keys {
-		fmt.Printf(format[output], key, variables[key])
+		fmt.Printf(format[*output], key, variables[key])
 	}
 }
 
@@ -148,14 +132,17 @@ func parser(in map[string]any, out map[string]string, root []string) {
 				switch any(value).(type) {
 				case map[string]any:
 					parser(value.(map[string]any), out, append(keys, fmt.Sprint(key)))
+
 				default:
-					out[fmt.Sprintf("%s%s%d", strings.Join(keys, separator), separator, key)] = fmt.Sprint(value)
+					out[fmt.Sprintf("%s%s%d", strings.Join(keys, *separator), *separator, key)] = fmt.Sprint(value)
 				}
 			}
+
 		case map[string]any:
 			parser(value.(map[string]any), out, keys)
+
 		default:
-			out[strings.Join(keys, separator)] = fmt.Sprint(value)
+			out[strings.Join(keys, *separator)] = fmt.Sprint(value)
 		}
 	}
 }
